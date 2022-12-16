3b67057330bc4834ab5706c2b14f67992dfc96a5
Егор Сивцев <gumben7@gmail.com>
2022-12-16 10:18:56 +0700

    final

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿namespace HelloWorld {
+﻿namespace HeloWorld {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int n = System.Int32.Parse(System.Console.ReadLine());
+            bool[] a = new bool[n + 1];
+            a[0] = true;
+            a[1] = true;
+            double sqrN = System.Math.Sqrt(n);
+            for (int i = 2; i <= sqrN; ++i) {
+                for (int s = i*i; s <= n; s += i) {
+                    a[s] = true;                   
+                }         
+            }
+            if (a[n]) {
+                System.Console.WriteLine("NO");
+                }
+            else {
+                System.Console.WriteLine("YES");
+            }
         }
     }
 }
